7a5fff75e235cb666ec290f47a36def2b53bbf2e
dannylty <dannylty98@gmail.com>
2024-04-02 23:29:11 +0800

    bugfix: Change debug to warning

--- modules/logger/base.py
+++ modules/logger/base.py
@@ -10,7 +10,7 @@ class BaseLogger:
             log_path = os.path.join(configs.log_path, video_id + ".log")
             with open(log_path, 'a+') as f:
                 pass
-            logging.basicConfig(filename=log_path, encoding='utf-8', level=logging.DEBUG)
+            logging.basicConfig(filename=log_path, encoding='utf-8', level=logging.WARNING)
             self.logger = logging.getLogger(name)
             self.logger.setLevel(level)
 
